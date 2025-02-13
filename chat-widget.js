b79a31ebc26cfc0f7e076f2d4ef4afd338a04994
joanfsalce <35925733+joanfsalce@users.noreply.github.com>
2025-02-12 23:52:37 -0500

    Update chat-widget.js

--- chat-widget.js
+++ chat-widget.js
@@ -260,7 +260,7 @@
         }
 
         .n8n-chat-widget .chat-footer a {
-            color: var(--chat--color-primary);
+            color: #000000;
             text-decoration: none;
             font-size: 12px;
             opacity: 0.8;
